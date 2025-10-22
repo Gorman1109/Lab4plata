03a902416fbe8b31a293be1127c79d280534f9e4
Adrian Mendez <adrianmendez1109@gmail.com>
2025-10-22 15:36:00 -0600

    Creacion de los tres ejemplos de las matrices a utilizar

--- ejercicio1.c
+++ ejercicio1.c
@@ -62,7 +62,57 @@ void procesar_matriz(int *mat, int rows, int cols) {
     putchar('\n');
 
     // Reconstruir y mostrar la matriz con las mismas dimensiones 
-    printf("\nMatriz reconstruida (%dx%d) después de ordenar:\n", rows, cols);
+    printf("\nMatriz reconstruida (%dx%d) despues de ordenar:\n", rows, cols);
     imp_matrix(mat, rows, cols);
     putchar('\n');
+}
+
+int main(void) {
+    /* Creacion de las tres matrices que se almacenan en buffers dinámicos para que sean contiguas y trabajemos con punteros.*/
+
+    /* Ejemplo 1: 2x3 */
+    int rows1 = 2, cols1 = 3;
+    int *mat1 = malloc(rows1 * cols1 * sizeof(int)); // aqui usamos malloc para garantizar memoria de forma contigua y permitir usar la misma funcion procesar_matriz para matrices de  distintas dimensiones
+
+    if (!mat1) { perror("malloc"); return 1; }
+    /* Llenado usando aritmética de punteros */
+    int vals1[] = { 9, 2, 7, 4, 5, 1 };
+    for (int k = 0; k < rows1 * cols1; ++k) {
+        *(mat1 + k) = *(vals1 + k);
+    }
+
+    /* Ejemplo 2: 3x3 */
+    int rows2 = 3, cols2 = 3;
+    int *mat2 = malloc(rows2 * cols2 * sizeof(int));
+    if (!mat2) { perror("malloc"); return 1; }
+    int vals2[] = { 12, 3, 5, 8, 1, 10, 6, 7, 2 };
+    for (int k = 0; k < rows2 * cols2; ++k) {
+        *(mat2 + k) = *(vals2 + k);
+    }
+
+    /* Ejemplo 3: 4x2 */
+    int rows3 = 4, cols3 = 2;
+    int *mat3 = malloc(rows3 * cols3 * sizeof(int));
+    if (!mat3) { perror("malloc"); return 1; }
+    int vals3[] = { 20, 4, 3, 15, 9, 0, 7, 11 };
+    for (int k = 0; k < rows3 * cols3; ++k) {
+        *(mat3 + k) = *(vals3 + k);
+    }
+
+    /* Procesar y mostrar cada caso */
+    printf("=== CASO 1 ===\n");
+    procesar_matriz(mat1, rows1, cols1);
+
+    printf("=== CASO 2 ===\n");
+    procesar_matriz(mat2, rows2, cols2);
+
+    printf("=== CASO 3 ===\n");
+    procesar_matriz(mat3, rows3, cols3);
+
+    /* Liberar memoria */
+    free(mat1);
+    free(mat2);
+    free(mat3);
+
+    return 0;
 }
